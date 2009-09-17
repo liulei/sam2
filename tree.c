5f7f3f9eabf8b0c89c10afec5acb8f402f43a7e4
liulei <liulei@shao.ac.cn>
2009-09-17 23:21:35 +0800

    second push.

---
+++ tree.c
@@ -0,0 +1,46 @@
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<math.h>
+
+#include	"allvars.h"
+#include	"proto.h"
+
+void read_tree(int treeNum){
+
+	if(halo != NULL){
+		free(halo);
+		halo	=	NULL;
+	}
+	halo	=	(HALO *)malloc(sizeof(HALO) * All.treeTable[treeNum]);
+	fread(halo, sizeof(HALO), All.treeTable[treeNum], All.fp);
+}
+
+void construct_tree(int treeNum){
+	int	i;
+	int	total	=	0;
+	int	count_per_snap[64];
+
+	for(i = 0; i < 64; ++i){
+		halo_per_snap[i]	=	0;
+		count_per_snap[i]	=	0;
+	}
+
+	for(i = 0; i < All.treeTable[treeNum]; ++i){
+		halo_per_snap[halo[i].snapNum]++;
+	}
+
+	snap	=	(SNAP **)malloc(sizeof(SNAP *) * 64);
+
+	for(i = 0; i < 64; ++i){
+		snap[i]	=	(SNAP *)malloc(sizeof(SNAP) * halo_per_snap[i]);
+		total	+=	halo_per_snap[i];
+	}
+
+	int	snapNum, count;
+	for(i = 0; i < All.treeTable[treeNum]; ++i){
+		snapNum	=	halo[i].snapNum;
+		count	=	count_per_snap[snapNum];
+		snap[snapNum][count].hid	=	i;
+		count_per_snap[snapNum]++;
+	}
+}
